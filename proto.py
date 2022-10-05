12ec7941108a7e1877124529f79c95d446486cd3
PierreBonnefoy <pierreeta@hotmail.fr>
2022-10-05 16:31:00 +0200

    email

--- proto.py
+++ proto.py
@@ -1,11 +1,16 @@
-#f=open("analyse/sfr_facture_fixe.txt",'r')
+f=open("analyse/sfr_facture_fixe.txt",'r')
 #f=open("analyse/sfr_facture_mobil.txt",'r')
-f=open("analyse/facture_EDF.txt",'r')
+#f=open("analyse/facture_EDF.txt",'r')
 
-tmp="start"
+# variable pour le montant
+tmpMontant="start"
 ligne = f.readline()
 montant = 0
 ttc = 0
+
+# variable pour l'email
+tmpEmail="ouioui@oui.oui"
+email = 0
 
 while(ligne != '') :
     ligne = ligne.split()
@@ -19,12 +24,22 @@ while(ligne != '') :
         # test de récup du prix
         if mot=='€' and montant and ttc:
             # enlève les caractères collés au prix
-            if tmp[0]!='0' and tmp[0]!='1' and tmp[0]!='2' and tmp[0]!='3' and tmp[0]!='4' and tmp[0]!='5' and tmp[0]!='6' and tmp[0]!='7' and tmp[0]!='8' and tmp[0]!='9' :
-                tmp=tmp[1:]
-            
+            if tmpMontant[0]!='0' and tmpMontant[0]!='1' and tmpMontant[0]!='2' and tmpMontant[0]!='3' and tmpMontant[0]!='4' and tmpMontant[0]!='5' and tmpMontant[0]!='6' and tmpMontant[0]!='7' and tmpMontant[0]!='8' and tmpMontant[0]!='9' :
+                tmpMontant=tmpMontant[1:]    
             # ICI MISE EN BDD
-            print(tmp)
-        tmp=mot
+            print(tmpMontant)
+        tmpMontant=mot
+
+        if mot == "email" :
+            email += 1
+        if email == 2 :
+            tmpEmail = mot
+            print(tmpEmail)
+            email = 0
+        if email == 1 and mot ==':' :
+            email += 1
+        
+        
     # passage à la ligne suivante
     ligne = f.readline()
     montant = 0
