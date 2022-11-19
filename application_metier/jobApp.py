17d37d47ce2b537f539a90ea157abe56f4fab66a
theflokky <aloyslanapro@gmail.com>
2022-11-19 14:27:26 +0100

    [FEAT] Fin UI

--- application_metier/jobApp.py
+++ application_metier/jobApp.py
@@ -53,22 +53,22 @@ def getMessage1(window, clientNameInput, startingDateInput, endingDateInput, ent
         # Launching JSON Generation Process
 
         awaitingResponse()
-        enterpriseList = readJson()
-        results1OnlyDateInterface(window)
+        total = readJson()
+        results1OnlyDateInterface(window, total, clientName,  startingDate, endingDate)
     elif (enterpriseName != '') and (startingDate != '' and endingDate != ''):
         print("Date and Enterprise Case")
         # Launching JSON Generation Process
 
         awaitingResponse()
-        enterpriseList =readJson()
-        results1AllInterface(window)
+        total =readJson()
+        results1AllInterface(window, total, clientName, startingDate, endingDate, enterpriseName)
     elif (enterpriseName != '') and (startingDate == '' and endingDate == ''):
         print("Enterprise case")
         # Launching JSON Generation Process
 
         awaitingResponse()
-        enterpriseList = readJson()
-        results1OnlyEnterpriseInterface(window)
+        total = readJson()
+        results1OnlyEnterpriseInterface(window, total, clientName, enterpriseName)
 
 
 def getMessage2(window, clientNameInput, productNameInput):
@@ -82,8 +82,8 @@ def getMessage2(window, clientNameInput, productNameInput):
 
         # Awaiting for the response
         awaitingResponse()
-        total = readJson()
-        results2Interface(window, total, clientName, productName)
+        enterpriseList = readJson()
+        results2Interface(window, enterpriseList, clientName, productName)
 
 def readJson():
     global currentFilePath
@@ -108,7 +108,8 @@ def readJson():
 
 def display():
     window = Tk()
-    window.geometry("1202x802")
+    window.geometry("1202x602")
+    window.title('Facturas')
     homeInterface(window)
     window.mainloop()
 
@@ -123,102 +124,211 @@ def homeInterface(window):
     message1Interface(window)
     message2Interface(window)
     resultsBaseInterface(window)
-    results2Interface(window, [65000], "Giroud", "Buts")
 
 
 def message1Interface(window):
     # Background of the Canvas
-    canvas = Canvas(window, width=600, height=600)
-    canvas.create_rectangle(0, 0, 600, 600, fill='black')
+    canvas = Canvas(window, width=600, height=400)
+    canvas.create_rectangle(0, 0, 600, 600, fill='#121212')
 
     # Form of the Message1
-    # clientName
-    clientNameLabel = Label(canvas, text="Nom Client :")
-    clientNameInput = Entry(canvas, bd=0)
+        #message1 title
+    titleLabel = Label(canvas, text='Message 1', width= 15, height=2, font=('Arial', 15), background='#121212', foreground='white')
+    titleLabel.place(x=220, y=10)
 
-    clientNameLabel.place(x=100, y=100)
-    clientNameInput.place(x=200, y=100)
+    #message description
+    descriptionLabel = Label(canvas, text='Vous retourne le total de vos factures sur une période et/ou pour une \nentreprise spécifique.', font=('Arial', 10), background='#121212', foreground='white')
+    descriptionLabel.place(x=100, y=70)
+
+    #Rectangle wich will contain the form
+    canvas.create_rectangle(150, 120, 450, 273, fill='#181818')
+    # clientName
+    clientNameLabel = Label(canvas, text="Nom du Client :", font=('Arial', 10), background="#121212", foreground='white')
+    clientNameInput = Entry(canvas, bd=0, font=('Arial', 10), background="#404040", foreground='white')
+
+    clientNameLabel.place(x=165, y=135)
+    clientNameInput.place(x=280, y=135)
 
     # starting date
-    startingDateLabel = Label(canvas, text="Date Début :")
-    startingDateInput = Entry(canvas, bd=0)
+    startingDateLabel = Label(canvas, text="Date de Début :", font=('Arial', 10), background="#121212", foreground='white')
+    startingDateInput = Entry(canvas, bd=0, font=('Arial', 10), background="#404040", foreground='white')
 
-    startingDateLabel.place(x=100, y=130)
-    startingDateInput.place(x=200, y=130)
+    startingDateLabel.place(x=165, y=170)
+    startingDateInput.place(x=280, y=170)
 
     # ending date
-    endingDateLabel = Label(canvas, text="Date Fin :")
-    endingDateInput = Entry(canvas, bd=0)
+    endingDateLabel = Label(canvas, text="Date de Fin :", font=('Arial', 10), background="#121212", foreground='white')
+    endingDateInput = Entry(canvas, bd=0, font=('Arial', 10), background="#404040", foreground='white')
 
-    endingDateLabel.place(x=100, y=160)
-    endingDateInput.place(x=200, y=160)
+    endingDateLabel.place(x=165, y=205)
+    endingDateInput.place(x=280, y=205)
 
     # enterprise name
-    enterpriseNameLabel = Label(canvas, text="Entreprise :")
-    enterpriseNameInput = Entry(canvas, bd=0)
+    enterpriseNameLabel = Label(canvas, text="N°SIREN :", font=('Arial', 10), background="#121212", foreground='white')
+    enterpriseNameInput = Entry(canvas, bd=0, font=('Arial', 10), background="#404040", foreground='white')
 
-    enterpriseNameLabel.place(x=100, y=190)
-    enterpriseNameInput.place(x=200, y=190)
+    enterpriseNameLabel.place(x=165, y=240)
+    enterpriseNameInput.place(x=280, y=240)
 
     # submit button
     submitButton = Button(canvas, text="Soumettre",
-                          command=lambda: getMessage1(window, clientNameInput, startingDateInput, endingDateInput, enterpriseNameInput))
-    submitButton.place(x=220, y=220)
+                          command=lambda: getMessage1(window, clientNameInput, startingDateInput, endingDateInput, enterpriseNameInput),
+                          width=15,
+                          height = 2)
+    submitButton.place(x=250, y=300)
 
     canvas.place(x=0, y=0)
 
 
 def message2Interface(window):
-    canvas = Canvas(window, width=600, height=600)
-    canvas.create_rectangle(0, 0, 600, 600, fill='black')
+    canvas = Canvas(window, width=600, height=400)
+    canvas.create_rectangle(0, 0, 600, 400, fill='#121212')
 
     # Form of the Message1
-    # clientName
-    clientNameLabel = Label(canvas, text="Nom Client :")
-    clientNameInput = Entry(canvas, bd=0)
+    #message1 title
+    titleLabel = Label(canvas, text='Message 2', width= 15, height=2, font=('Arial', 15), background='#121212', foreground='white')
+    titleLabel.place(x=220, y=10)
 
-    clientNameLabel.place(x=100, y=100)
-    clientNameInput.place(x=200, y=100)
+    #message description
+    descriptionLabel = Label(canvas, text='Vous retourne la liste des entreprises vous ayant vendu ce produit.', font=('Arial', 10), background='#121212', foreground='white')
+    descriptionLabel.place(x=100, y=70)
+
+    #Rectangle wich will contain the form
+    canvas.create_rectangle(150, 120, 450, 250, fill='#181818')
+
+    # clientName
+    clientNameLabel = Label(canvas, text="Nom du client :", font=('Arial', 10), background="#121212", foreground='white')
+    clientNameInput = Entry(canvas, bd=0, font=('Arial', 10), background="#404040", foreground='white')
+
+    clientNameLabel.place(x=165, y=150)
+    clientNameInput.place(x=280, y=150)
 
     # product name
-    productNameLabel = Label(canvas, text="Produit :")
-    productNameInput = Entry(canvas, bd=0)
+    productNameLabel = Label(canvas, text="Produit Visé :", font=('Arial', 10), background="#121212", foreground='white')
+    productNameInput = Entry(canvas, bd=0, font=('Arial', 10), background="#404040", foreground='white')
 
-    productNameLabel.place(x=100, y=130)
-    productNameInput.place(x=200, y=130)
+    productNameLabel.place(x=165, y=200)
+    productNameInput.place(x=280, y=200)
 
     # submit button
     submitButton = Button(canvas, text="Soumettre",
-                          command=lambda: getMessage2(window, clientNameInput, productNameInput))
-    submitButton.place(x=220, y=220)
+                          command=lambda: getMessage2(window, clientNameInput, productNameInput),
+                          width=15,
+                          height = 2)
+    submitButton.place(x=250, y=300)
     canvas.place(x=600, y=0)
 
 
 def resultsBaseInterface(window):
     canvas = Canvas(window, width=1200, height=200)
-    canvas.create_rectangle(0, 0, 1200, 200, fill='black')
-    canvas.place(x=0, y=600)
+    canvas.create_rectangle(0, 0, 1200, 200, fill='#121212')
 
-def results1OnlyDateInterface(window):
-    canvas = Canvas(window, width=1200, height=200)
-    canvas.create_rectangle(0, 0, 1200, 200, fill='red')
-    canvas.place(x=0, y=600)
+    canvas.create_rectangle(150, 60, 450, 170, fill='#181818')
+    title1Label = Label(canvas, text='Requête', width= 15, height=1, font=('Arial', 15), background='#121212', foreground='white')
+    title1Label.place(x=220, y=10)
 
-def results1OnlyEnterpriseInterface(window):
-    canvas = Canvas(window, width=1200, height=200)
-    canvas.create_rectangle(0, 0, 1200, 200, fill='green')
-    canvas.place(x=0, y=600)
+    canvas.create_rectangle(750, 60, 1050, 170, fill='#181818')
+    title2Label = Label(canvas, text='Résultat', width= 15, height=1, font=('Arial', 15), background='#121212', foreground='white')
+    title2Label.place(x=820, y=10)
+    
+    canvas.place(x=0, y=400)
+    return canvas
 
-def results1AllInterface(window):
-    canvas = Canvas(window, width=1200, height=200)
-    canvas.create_rectangle(0, 0, 1200, 200, fill='orange')
-    canvas.place(x=0, y=600)
+def results1OnlyDateInterface(window, total, clientName, startingDate, endingDate):
+    canvas = resultsBaseInterface(window)
+
+    clientLabel = Label(canvas, text="Nom du client :", font=('Arial', 10), background="#121212", foreground='white')
+    startingDateLabel = Label(canvas, text="Date de Début :", font=('Arial', 10), background="#121212", foreground='white')
+    endingDateLabel = Label(canvas, text="Date de Fin :", font=('Arial', 10), background="#121212", foreground='white')
+    clientLabel.place(x = 165, y = 70)
+    startingDateLabel.place(x = 165, y = 105)
+    endingDateLabel.place(x = 165, y = 140)
+
+    clientNameLabel = Label(canvas, text=clientName, font=('Arial', 10), background="#121212", foreground='white')
+    startingDateNameLabel = Label(canvas, text=startingDate, font=('Arial', 10), background="#121212", foreground='white')
+    endingDateNameLabel = Label(canvas, text=endingDate, font=('Arial', 10), background="#121212", foreground='white')
+    clientNameLabel.place(x = 280, y = 70)
+    startingDateNameLabel.place(x = 280, y = 105)
+    endingDateNameLabel.place(x = 280, y = 140)
+
+    totalNameLabel = Label(canvas, text='Total :', font=('Arial', 15), background="#121212", foreground='white')
+    totalNameLabel.place(x = 780, y = 100)
+
+    totalLabel = Label(canvas, text=str(total[0]), font=('Arial', 15), background="#121212", foreground='white')
+    totalLabel.place(x = 900, y = 100)
+
+def results1OnlyEnterpriseInterface(window, total, clientName, enterpriseName):
+    canvas = resultsBaseInterface(window)
+
+    clientLabel = Label(canvas, text="Nom du client :", font=('Arial', 10), background="#121212", foreground='white')
+    productLabel = Label(canvas, text="Entreprise Visée :", font=('Arial', 10), background="#121212", foreground='white')
+    clientLabel.place(x = 165, y = 80)
+    productLabel.place(x = 165, y = 120)
+
+    clientNameLabel = Label(canvas, text=clientName, font=('Arial', 10), background="#121212", foreground='white')
+    productNameLabel = Label(canvas, text=enterpriseName, font=('Arial', 10), background="#121212", foreground='white')
+    clientNameLabel.place(x = 280, y = 80)
+    productNameLabel.place(x = 280, y = 120)
+
+    totalNameLabel = Label(canvas, text='Total :', font=('Arial', 15), background="#121212", foreground='white')
+    totalNameLabel.place(x = 780, y = 100)
+
+    totalLabel = Label(canvas, text=str(total[0]), font=('Arial', 15), background="#121212", foreground='white')
+    totalLabel.place(x = 900, y = 100)
+
+def results1AllInterface(window, total, clientName, startingDate, endingDate, enterpriseName):
+    canvas = resultsBaseInterface(window)
+
+    clientLabel = Label(canvas, text="Nom du client :", font=('Arial', 10), background="#121212", foreground='white')
+    startingDateLabel = Label(canvas, text="Date de Début :", font=('Arial', 10), background="#121212", foreground='white')
+    endingDateLabel = Label(canvas, text="Date de Fin :", font=('Arial', 10), background="#121212", foreground='white')
+    enterpriseNameLabel = Label(canvas, text="Entreprise Visée :", font=('Arial', 10), background="#121212", foreground='white')
+    clientLabel.place(x = 165, y = 70)
+    startingDateLabel.place(x = 165, y = 95)
+    endingDateLabel.place(x = 165, y = 120)
+    enterpriseNameLabel.place(x = 165, y = 145)
+
+
+    clientNameLabel = Label(canvas, text=clientName, font=('Arial', 10), background="#121212", foreground='white')
+    startingDateNameLabel = Label(canvas, text=startingDate, font=('Arial', 10), background="#121212", foreground='white')
+    endingDateNameLabel = Label(canvas, text=endingDate, font=('Arial', 10), background="#121212", foreground='white')
+    enterpriseLabel = Label(canvas, text=enterpriseName, font=('Arial', 10), background="#121212", foreground='white')
+    clientNameLabel.place(x = 280, y = 70)
+    startingDateNameLabel.place(x = 280, y = 95)
+    endingDateNameLabel.place(x = 280, y = 120)
+    enterpriseLabel.place(x = 280, y = 145)
+
+    totalNameLabel = Label(canvas, text='Total :', font=('Arial', 15), background="#121212", foreground='white')
+    totalNameLabel.place(x = 780, y = 100)
+
+    totalLabel = Label(canvas, text=str(total[0]), font=('Arial', 15), background="#121212", foreground='white')
+    totalLabel.place(x = 900, y = 100)
 
 #Function wich is displaying the results of the number 2 request
-def results2Interface(window, total, clientName, productName):
-    clientNameLabel = Label(window, text=clientName)
-    productNameLabel = Label(window, text=productName)
-    totalLabel = Label(window, text=str(total[0]))
-    totalLabel.place(x = 50, y = 650)
-    clientNameLabel.place(x = 50, y = 700)
-    productNameLabel.place(x = 50, y = 750)
+def results2Interface(window, enterpriseList, clientName, productName):
+    canvas = resultsBaseInterface(window)
+
+    clientLabel = Label(canvas, text="Nom du client :", font=('Arial', 10), background="#121212", foreground='white')
+    productLabel = Label(canvas, text="Produit Visé :", font=('Arial', 10), background="#121212", foreground='white')
+    clientLabel.place(x = 165, y = 80)
+    productLabel.place(x = 165, y = 120)
+
+    clientNameLabel = Label(canvas, text=clientName, font=('Arial', 10), background="#121212", foreground='white')
+    productNameLabel = Label(canvas, text=productName, font=('Arial', 10), background="#121212", foreground='white')
+    clientNameLabel.place(x = 280, y = 80)
+    productNameLabel.place(x = 280, y = 120)
+
+    enterpriseLabel = Label(canvas, text='Liste :', font=('Arial', 15), background="#121212", foreground='white')
+    enterpriseLabel.place(x = 780, y = 100)
+
+    scroll = Scrollbar(canvas, activebackground="#181818")
+    scroll.place(x=1010, y=90)
+
+    enterpriseNameLabel = Listbox(canvas, font=('Arial', 10), background="#121212", foreground='white', yscrollcommand=scroll.set, width=15, height=4)
+    enterpriseNameLabel.place(x = 900, y = 80)
+    for i in range(0, len(enterpriseList)):
+        enterpriseNameLabel.insert(i, enterpriseList[i])
+
+
+
+
